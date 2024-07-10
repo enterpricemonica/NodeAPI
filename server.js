2bb1cdd46340291cce4f96ec0330d27d6c4ca578
Monica Santamaria <santamariamonica31@gmail.com>
2024-07-10 16:20:21 -0500

    Tres: Hola mundo express

--- server.js
+++ server.js
@@ -1,1 +1,15 @@
-console.log("SOFTWARE: ADSO 2024")
+const express = require('express');
+
+// Crear una instancia de Express
+const app = express();
+
+// Definir una ruta de prueba
+app.get('/', (req, res) => {
+  res.send('Hola mundo: ADSO 2024');
+});
+
+// Configurar el puerto en el que escuchará el servidor
+const PORT = 5000;
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en el puerto ${PORT}`);
+});
